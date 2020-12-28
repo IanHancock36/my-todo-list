e0f509ad602ed2467d0e7d6332e44555d4f3a43a
Ian Hancock <northroadian@gmail.com>
2020-12-28 07:23:02 -0600

    added create Todo to forms

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,47 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class NewTodoForm extends Component {
-     constructor(props){
-         super(props);
-         this.state = { task: ''}
-     }
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-     handleChange(evt){
-         this.setState({
-             
-         })
-
-     }
-
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+//you pass in this.state since you need the {task: ""} its statefull
+// since you create new tasks but its a prop since you will pass these in. 
+// you put this.setState to the empty string task so it resets and doe not
+// keep what you typed in the to do list input form.   
+handleSubmit(evt){
+      evt.preventDefault();
+      this.props.createTodo(this.state)
+      this.setState ({task: ''}); 
 
     
-     render() {
-        return (
-            <form>
-                <label htmlFor = "task">New Todo </label>
-                <input 
-                type= "text" 
-                placeholder= "New Todo" 
-                id='task'
-                value ={this.state.task} 
-                onChange = {this.handleChange}/> 
+  }
 
-
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="task">New Todo </label>
+        <input
+          type="text"
+          placeholder="New Todo"
+          id="task"
+          value={this.state.task}
+          onChange={this.handleChange}
+          name="task"
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
 }
 
-export default NewTodoForm 
-// #1 there is state involved since form will change thats why there is a constructor used. 
+export default NewTodoForm;
+// #1 there is state involved since form will change thats why there is a constructor used.
 
-// #2 state #1 set your state to empty string of {task: "" } since its an empty object 
+// #2 state #1 set your state to empty string of {task: "" } since its an empty object
 
-// #3 add in {this.state.task} for the 'value = ' of your input 
+// #3 add in {this.state.task} for the 'value = ' of your input
 
 //#4 now you need a onChange and put the handleChange in for the event.
 // then you build handleChange passing event and set the state of the handleChange
 
+//#5 to make a new todo item pop up to accomplish you must add a "onSubmit"
+// on the form.
+
+//#6 call the new handleSubmit. then give it a function to call it. 
+// go back into the Todo list and call a create function.
+
+
 
 //In your htmlFor and the Id it needs to be the same thing for html reading purposes
-// if the page breaks and or somone is using a screen reader since they are sight impared. 
+// if the page breaks and or somone is using a screen reader since they are sight impared.
--- src/TodoList.js
+++ src/TodoList.js
@@ -5,17 +5,30 @@ class TodoList extends Component {
     constructor(props){
         super(props);
         this.state = {todos: [{task: "Walk Tiger"}, {task: "Feed Dinosaur"} ]
-    
+        
     }; 
+    this.create = this.create.bind(this); 
     }
-// making new variable called todos and mapping the todo component. 
-    render() {
+// create (newtoDo) this makes new State it takes the new todos
+// and then it adds the newTodo to the list.
+//then you have to bind it. 
+//then you pass it to the NewTodoForm below. using "createTodo = {this.create}" 
+// then you pass {this.create} in the actual NewTodoForm as props in the handleSubmit
+create(newTodo){
+    this.setState({
+        todos: [...this.state.todos , newTodo]
+    })
+
+}    
+// making new variable called todos and mapping the todo component.
+render() {
         const todos = this.state.todos.map(todo => {
             return <Todo task = {todo.task} />
         })
         return (
             <div>
             <h1> Todo List! </h1>
+            <NewTodoForm createTodo = {this.create}  />
             <ul>
             {todos}
             </ul>
